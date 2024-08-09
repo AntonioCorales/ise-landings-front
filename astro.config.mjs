0aa76d78fbd30d781565a1f4f0546325f73e359d
AntonioCorales <70595016+AntonioCorales@users.noreply.github.com>
2024-08-09 18:54:08 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import vercelServerless from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],  
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercelServerless(),
 });
